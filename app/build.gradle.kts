0d17bd46628f126942c346b774c44b31e746f58a
Venkatesan A <venkatesanap142@gmail.com>
2024-05-22 08:39:25 +0530

    versionCode changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.appsmindstudio.readinnews"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
